4fe4496522c6b915fccbd81aa02abcd4c454acb9
ericy99 <71351524+ericy99@users.noreply.github.com>
2024-05-10 12:28:12 -0400

    Update Weather.java

--- src/main/java/com/example/demo/Weather.java
+++ src/main/java/com/example/demo/Weather.java
@@ -36,4 +36,14 @@ public class Weather {
     public void setIcon(String icon) {
         this.icon = icon;
     }
+
+    public void mySqlQuery(String user, String pass ) {
+        String user = request.getParameter("user");
+        String pass = request.getParameter("pass");
+        String query = "SELECT user_id FROM user_data WHERE user_name = '" + user + "' and user_password = '" + pass +"'";
+        try {
+            Statement statement = connection.createStatement( 
+            ResultSet results = statement.executeQuery( query ); // Unsafe!    
+        }
+    }
 }
